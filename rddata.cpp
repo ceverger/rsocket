1ae7d5bec64838bc30acfcb52684c1908eea0589
ceverger <74200769+ceverger@users.noreply.github.com>
2024-03-17 12:55:26 +0300

    Update rddata.cpp

--- rddata.cpp
+++ rddata.cpp
@@ -5,11 +5,20 @@
 #include <unistd.h>
 #include "rsocket.hpp"
 
-	int RSocket::rddata(char *buf, int size)
-	{
-		if(state != opened) return -1;
-		if(buf == nullptr)  return -1;		
-		int ret = read(fd, buf, size);		
-		if(ret < 0) errstr = strerror(errno);
-		return ret;	
-	}
+        int RSocket::rddata(char *buf, int size)
+        {
+                if(state != connected)
+                {  errstr = rddata_error_state;
+                        return -1;
+                }
+
+                if(buf == nullptr)
+                {
+                        errstr = rddata_error_buffer;
+                        return -1;
+                }
+
+                int ret = read(fd, buf, size);
+                if(ret < 0) errstr = strerror(errno);
+                return ret;
+        }
